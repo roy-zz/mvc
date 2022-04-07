3d42a1e81dbf9e2650bceecdd53ae76c8fdae25c
roy-zz <74298434+roy-zz@users.noreply.github.com>
2022-04-07 15:58:06 +0900

    setting multi modules

---
+++ servlet/src/test/java/com/roy/mvc/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roy.mvc.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
