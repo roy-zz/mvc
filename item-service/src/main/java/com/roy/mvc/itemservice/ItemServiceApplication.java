98a607156f789e40548b9fc60595e2923141daa7
roy-zz <74298434+roy-zz@users.noreply.github.com>
2022-04-15 17:14:06 +0900

    item-service 모듈 추가

---
+++ item-service/src/main/java/com/roy/mvc/itemservice/ItemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.roy.mvc.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class, args);
+    }
+
+}
